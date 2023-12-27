d24589c6f9d22394191e8edf59fa0052c15d1063
foltone <foltonedev@gmail.com>
2023-12-27 15:23:18 +0100

    add database populating

--- db.sql
+++ db.sql
@@ -20,3 +20,24 @@ CREATE TABLE Incidents (
    date_end DATETIME NOT NULL,
    FOREIGN KEY (escalator_id) REFERENCES Escalators(id)
 );
+
+
+INSERT INTO Users (email, password, role) VALUES
+    ('user1@example.com', 'password1', 'admin'),
+    ('user2@example.com', 'password2', 'operator'),
+    ('user3@example.com', 'password3', 'user')
+;
+
+
+INSERT INTO Escalators (name, status) VALUES
+    ('Escalator A', 'operational'),
+    ('Escalator B', 'under maintenance'),
+    ('Escalator C', 'out of order')
+;
+
+
+INSERT INTO Incidents (escalator_id, description, status, date_begin, date_end) VALUES
+    (1, 'Technical issue', 'pending', '2023-01-01 08:00:00', '2023-01-01 09:30:00'),
+    (2, 'Routine maintenance', 'resolved', '2023-02-15 10:00:00', '2023-02-15 12:00:00'),
+    (3, 'Major malfunction', 'in progress', '2023-03-20 15:45:00', '2023-03-21 10:00:00')
+;
