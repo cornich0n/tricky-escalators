e64753fdff9ada823baf28992728b0b2fd822b5d
foltone <foltonedev@gmail.com>
2023-12-27 15:25:08 +0100

    add escalator router

--- routes/escalatorRouter.js
+++ routes/escalatorRouter.js
@@ -3,19 +3,19 @@ const Escalator = require("../models/Escalator");
 const checkAuth = require("../middlewares/checkAuth");
 const router = new Router();
 
-router.get("/users", checkAuth({ transient: true }), async (req, res, next) => {
-    if (req.Escalator) {
-        req.query.id = req.Escalator.id;
+router.get("/escalators", checkAuth({ transient: true }), async (req, res, next) => {
+    if (req.SuperUser) {
+        req.query.id = req.SuperUser.id;
     }
-    const users = await Escalator.findAll({
+    const escalators = await Escalator.findAll({
         where: req.query,
     });
-    res.json(users);
+    res.json(escalators);
 });
 
 router.use(checkAuth());
 
-router.post("/users", async (req, res, next) => {
+router.post("/escalators", async (req, res, next) => {
     try {
         res.status(201).json(await Escalator.create(req.body));
     } catch (error) {
@@ -23,14 +23,14 @@ router.post("/users", async (req, res, next) => {
     }
 });
 
-router.get("/users/:id", async (req, res, next) => {
-    if (req.Escalator.id !== parseInt(req.params.id)) return res.sendStatus(403);
+router.get("/escalators/:id", async (req, res, next) => {
+    if (req.SuperUser.id !== parseInt(req.params.id)) return res.sendStatus(403);
     const Escalator = await Escalator.findByPk(parseInt(req.params.id));
     if (!Escalator) res.sendStatus(404);
     else res.json(Escalator);
 });
 
-router.put("/users/:id", async (req, res, next) => {
+router.put("/escalators/:id", async (req, res, next) => {
     try {
         const result = await Escalator.destroy({
             where: {
@@ -48,33 +48,28 @@ router.put("/users/:id", async (req, res, next) => {
     }
 });
 
-router.delete("/users/:id", async (req, res, next) => {
+router.delete("/escalators/:id", async (req, res, next) => {
     const result = await Escalator.destroy({
         where: {
             id: parseInt(req.params.id),
         },
     });
-
     res.sendStatus(result ? 204 : 404);
 });
 
-router.patch("/users/:id", async (req, res, next) => {
-    try {
-        const [nbUpdated] = await Escalator.update(req.body, {
-            where: {
-                id: parseInt(req.params.id),
-            },
-            individualHooks: true,
-        });
-        const Escalator = await Escalator.findByPk(parseInt(req.params.id));
-        if (Escalator) {
-            res.status(200).json(Escalator);
-        } else {
-            res.sendStatus(404);
-        }
-    } catch (e) {
-        next(e);
-    }
+router.get("/escalators/:id/incidents", async (req, res, next) => {
+    const Escalator = await Escalator.findByPk(parseInt(req.params.id));
+    if (!Escalator) return res.sendStatus(404);
+    res.json(await Escalator.getIncidents());
+});
+
+router.post("/escalators/:id/incidents", async (req, res, next) => {
+    const Escalator = await Escalator.findByPk(parseInt(req.params.id));
+    if (!Escalator) return res.sendStatus(404);
+    const incident = await Incident.findByPk(req.body.id);
+    if (!incident) return res.sendStatus(404);
+    await Escalator.addIncident(incident);
+    res.sendStatus(201);
 });
 
 module.exports = router;
